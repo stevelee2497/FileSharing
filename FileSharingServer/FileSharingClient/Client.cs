4b7455151e8b6a0755182ad1a41be8e463ef48ce
Steven <stevelee2497@gmail.com>
2018-11-06 19:04:55 +0700

    Init project

---
+++ FileSharingServer/FileSharingClient/Client.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace FileSharingClient
+{
+	public class Client
+	{
+		private const int PortNumber = 9999;
+
+		public static void Main(string[] args)
+		{
+			{
+				try
+				{
+					TcpClient client = new TcpClient();
+
+					// 1. connect
+					client.Connect("127.0.0.1", PortNumber);
+					Stream stream = client.GetStream();
+
+					Console.WriteLine("Connected to Y2Server.");
+					while (true)
+					{
+						Console.Write("Enter your name: ");
+
+						string str = Console.ReadLine();
+						var reader = new StreamReader(stream);
+						var writer = new StreamWriter(stream) {AutoFlush = true};
+
+						// 2. send
+						writer.WriteLine(str);
+
+						// 3. receive
+						str = reader.ReadLine();
+						Console.WriteLine(str);
+						if (str?.ToUpper() == "BYE")
+							break;
+					}
+					// 4. close
+					stream.Close();
+					client.Close();
+				}
+
+				catch (Exception ex)
+				{
+					Console.WriteLine("Error: " + ex);
+				}
+
+				Console.Read();
+			}
+			
+		}
+	}
+}
---
+++ FileSharingServer/FileSharingServer/Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace FileSharingServer
+{
+	public class Server
+	{
+		private const int PortNumber = 9999;
+
+		public static void Main(string[] args)
+		{
+			var listener = new TcpListener(IPAddress.Parse("127.0.0.1"), PortNumber);
+			listener.Start();
+
+			Console.WriteLine("Server started on " + listener.LocalEndpoint);
+			Console.WriteLine("Waiting for a connection...");
+
+			var socket = listener.AcceptSocket();
+			Console.WriteLine("Connection received from " + socket.RemoteEndPoint);
+
+			var stream = new NetworkStream(socket);
+			var reader = new StreamReader(stream);
+			var writer = new StreamWriter(stream) { AutoFlush = true };
+
+			var str = reader.ReadToEnd();
+
+			writer.WriteLine("Hello " + str);
+
+			stream.Close();
+			socket.Close();
+			Console.Read();
+		}
+	}
+}
