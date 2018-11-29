8cea3de0a07c001fca75e8351d1042dd01a020b9
Steven <stevelee2497@gmail.com>
2018-11-29 20:56:40 +0700

    Auto stash before merge of "master" and "origin/master"

---
+++ FileSharingApp/FileSharingApp/Views/LoginView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace FileSharingApp.Views
+{
+	[Activity(Label = "LoginView", Theme = "@style/AppTheme.Fullscreen")]
+	public class LoginView : AppCompatActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+		}
+	}
+}
